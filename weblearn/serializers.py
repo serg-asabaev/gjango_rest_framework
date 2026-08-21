0dfb068becc371f71495d0f9143a3f2b0bb16deb
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-08-21 22:04:27 +0400

    added

--- weblearn/serializers.py
+++ weblearn/serializers.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from users.models import Payment
 from weblearn.models import LearnCourse, Lesson
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ('id', 'title', 'description', 'learn_course')
+
 class LearnCourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
-    lesson_list = serializers.SerializerMethodField()
+    lesson_list = LessonSerializer(source="lessons", many=True)
 
     class Meta:
         model = LearnCourse
@@ -14,23 +19,6 @@ class LearnCourseSerializer(serializers.ModelSerializer):
     def get_lesson_count(self, instance):
         lessons = instance.lessons.all()
         return lessons.count()
-
-    def get_lesson_list(self, instance):
-        lessons = instance.lessons.all()
-        lessons_list = []
-        for lesson in lessons:
-            lessons_list.append({
-                "title": f"{lesson.title}",
-                "description": f"{lesson.description}"
-            })
-        return lessons_list
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = ('id', 'title', 'description', 'learn_course')
-
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
