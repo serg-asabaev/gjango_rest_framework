e412eca2b1a877ee9867295aaadb0d0e260de5c7
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-09-03 22:36:34 +0400

    added settings for CORS and swagger

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,9 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "rest_framework_simplejwt",
+    "drf_yasg",
+    "corsheaders",
+
     "users",
     "weblearn",
 ]
@@ -43,6 +46,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -144,3 +148,11 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    '<http://localhost:8080>',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8080",
+]
